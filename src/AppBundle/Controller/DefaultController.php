437bc43fb569731bd43dd18273c6019567da9cbe
giansalex <giansalex@gmail.com>
2016-12-09 10:11:35 -0500

    Fix AUTH control access

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -5,13 +5,11 @@ namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 
 class DefaultController extends Controller
 {
     /**
      * @Route("/", name="homepage")
-     * @Security("has_role('ROLE_USER')")
      */
     public function indexAction(Request $request)
     {
